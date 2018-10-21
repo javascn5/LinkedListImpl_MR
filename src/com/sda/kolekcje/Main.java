e304b1a74f4ceec4550673386fccea724257ccdf
helgorn <rusiecki.michal@gmail.com>
2018-10-21 13:38:09 +0200

    Interfejs i klasa

--- src/com/sda/kolekcje/Main.java
+++ src/com/sda/kolekcje/Main.java
@@ -1,8 +1,19 @@
 package com.sda.kolekcje;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<String> list = new LinkedList<>();
+
+        list.add("Jan");
+        list.add("Jacek");
+        list.add("Michal");
+
+        for(String l: list){
+            System.out.println(l);
+        }
     }
 }
